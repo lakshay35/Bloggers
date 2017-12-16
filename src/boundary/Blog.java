10558a953ead77f28ff3f5ac0d4c8b567c37b79d
lakshaysharma <lakshay35@gmail.com>
2017-12-16 12:20:03 -0500

    New Project

---
+++ src/boundary/Blog.java
@@ -0,0 +1,24 @@
+package boundary;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/Blog")
+public class Blog extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String fname = request.getParameter("fname");
+        String lname = request.getParameter("lname");
+        String email = request.getParameter("email");
+        String phone = request.getParameter("phone");
+        String responseText = "Hello, " + fname + " " + lname + ". According to our records your email is " + email + ", and your phone number is " + phone;
+        response.getWriter().write(responseText);
+    }
+}
